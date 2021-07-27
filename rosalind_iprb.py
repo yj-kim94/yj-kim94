3f7cb47a7ef9a0b53e9a4f339b702f78fc0f6aef
yj-kim94 <dydwls2061@naver.com>
2021-07-27 19:52:48 +0900

    Adjust rosalind_iprb.py

--- rosalind_iprb.py
+++ rosalind_iprb.py
@@ -5,12 +5,12 @@ os.chdir("C://Users/김용진/Documents/Git")
 with open("rosalind_iprb.txt","r") as f:
   line=f.read()
 
-hom, het, rec = map(int, line.split())
+dom, het, rec = map(int, line.split())
 
 #전체 발생 가능한 자손의 개수 구하기
 
 #전체 중 2개씩 뽑아 mate를 이룰 경우의 수(조합)
-total_comb=comb(hom+het+rec,2)
+total_comb=comb(dom+het+rec,2)
 
 #각 mate로부터 4개 조합의 자손이 나온다.
 total_children=4*total_comb
